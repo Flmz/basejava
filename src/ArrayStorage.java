14b985e74133358eca3351de9dc796470ea22a07
Flmz <miy2h@yandex.ru>
2022-06-25 00:00:01 +0300

    I'm adding a modified method based on Maxim's comment

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,32 +5,32 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null); //
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
-
 
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r; // Вставляем пришедшее нам резюме в первую пустую ячейку.
+                size++;
                 return;
             }
         }
     }
 
     Resume get(String uuid) {
-
-        for (Resume resume : storage) {
-            if (resume == null) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
                 System.out.println("No resume with such id");
                 break;
             } else {
-                if (uuid.equals(resume.uuid))
-                    return resume;
+                if (uuid.equals(storage[i].uuid))
+                    return storage[i];
             }
-
         }
         return null;
     }
@@ -42,28 +42,25 @@ public class ArrayStorage {
             } else {
                 if (uuid.equals(storage[i].uuid)) {
                     storage[i] = null;
+                    System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);//сдвигаем наш массив,
+                    //чтобы не было пустых ячеек между id
+                    size--;
                     break;
                 }
             }
-
         }
-
-        //Двигаем наши резюме налево, чтобы между ними не было пустых ячеек.
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) {
-                Resume a = storage[i];
-                storage[i] = storage[i + 1];
-                storage[i + 1] = a;
-
-            }
-        }
-
     }
 
+    void print() {
+        for (int i = 0; i < storage.length - 1; i++) {
+            System.out.println(storage[i]);
+        }
+    }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     Resume[] getAll() {
         Resume[] copyResume = new Resume[size()];
 
@@ -79,16 +76,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int counter = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                counter++;
-            } else if (i > counter + 1) { // Так как наши резюме хранятся без пустот между друг другом, нам не нужно
-                //перебирать весь массив, чтобы узнать его размер.
-                break;
-            }
-        }
-
-        return counter;
+        return size;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -5,6 +5,7 @@ public class Resume {
 
     // Unique identifier
     String uuid;
+    int size;
 
     @Override
     public String toString() {
